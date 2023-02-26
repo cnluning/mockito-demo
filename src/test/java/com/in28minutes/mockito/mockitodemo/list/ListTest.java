d5f5a31d61d8e6cb5521a3d96eea5670825920ba
Claire Luning <cnluning@gmail.com>
2023-02-26 16:28:20 -0600

    Exploring mocks further by using MockingList interface

---
+++ src/test/java/com/in28minutes/mockito/mockitodemo/list/ListTest.java
@@ -0,0 +1,40 @@
+package com.in28minutes.mockito.mockitodemo.list;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.awt.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class ListTest {
+	
+	@Test
+	void simpleTest() {
+		List listMock = mock(List.class);
+		//listMock.size() => 3
+		when(listMock.size()).thenReturn(3);
+		assertEquals(3, listMock.size());
+	}
+	
+	@Test
+	void specificParameters() {
+		List listMock = mock(List.class);
+		//listMock.size() => 3
+		when(listMock.get(0)).thenReturn("SomeString");
+		assertEquals("SomeString", listMock.get(0));
+		assertEquals(null, listMock.get(1));
+	}
+	
+	@Test
+	void genericParameters() {
+		List listMock = mock(List.class);
+		//listMock.size() => 3
+		when(listMock.get(Mockito.anyInt())).thenReturn("SomeOtherString");
+		assertEquals("SomeOtherString", listMock.get(0));
+		assertEquals("SomeOtherString", listMock.get(1));
+	}
+
+}
